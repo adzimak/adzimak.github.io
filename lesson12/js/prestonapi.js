0311cfe737f97bda89454c515953b48e5fb33f50
adzimak <adz13004@byui.edu>
2021-09-13 11:27:40 -0600

    commit

---
+++ lesson12/js/prestonapi.js
@@ -0,0 +1,23 @@
+//Selecting city
+if (document.getElementById("preston")) {
+apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a55613932a68b64904c80b978dc0e96f&units=imperial';
+} else if (document.getElementById("sodasprings")) {
+apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=a55613932a68b64904c80b978dc0e96f&units=imperial';
+} else if (document.getElementById("fishhaven")) {
+apiURL = 'https://api.openweathermap.org/data/2.5/weather?lat=42.0380399&lon=-111.4048681&appid=a55613932a68b64904c80b978dc0e96f&units=imperial';
+}
+
+//Usign fetch to feed the argument
+fetch(apiURL)
+.then((response) => response.json())
+.then((jsObject) => {
+console.log(jsObject);
+
+//Calling the JS in the <span> tag in the HTML to print the current data
+document.getElementById('description').textContent = jsObject.weather[0].description;
+document.getElementById('temp').textContent = jsObject.main.temp;
+document.getElementById('humidity').textContent = jsObject.main.humidity;
+document.getElementById('speed').textContent = jsObject.wind.speed;
+
+getWindChill();
+});
---
+++ lesson12/js/windchill.js
@@ -0,0 +1,18 @@
+function getWindChill() {
+// 1 Getting the variables values: Input
+var temp = document.getElementById("temp").textContent;
+var windspeed = document.getElementById("speed").textContent;
+        
+// 2 Formula to calculate the wind chill
+var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16));
+        
+// 3 Round the output value
+var windChill= Math.round(windChill);
+        
+// 4 Input requirements: Windchill temperature is below 50 °F and wind speeds above 3.0 mph
+if (temp <= 50 && windspeed >= 3) {
+document.getElementById("windChill").textContent = windChill;
+} else {
+document.getElementById("windChill").innerHTML = "N/A";
+}
+}
