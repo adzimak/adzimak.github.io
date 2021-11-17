2f9326d9ffef8263f8e46a4076fae109f8b7fff1
kadzima <adz13004@byui.edu>
2021-11-17 16:14:14 -0600

    initial commit

---
+++ lesson2/weather/js/message.js
@@ -0,0 +1,7 @@
+function getday(){
+var day = new Date();
+if( day.toString().substr(0, 6) === 'Sat'){
+document.getElementById('day').innerHTML = "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion";
+}
+}
+getday();
